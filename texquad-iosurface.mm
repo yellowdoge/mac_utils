3a532ed56340e00b86c31d048bf7265ffc58260c
Miguel Casas-Sanchez <mcasas@chromium.org>
2018-01-04 18:36:04 -0500

    Added texquad-iosurface.mm from https://crbug.com/510087 and manipulated to make it work using just glClearColor().

---
+++ texquad-iosurface.mm
@@ -0,0 +1,188 @@
+// Compile with:
+// clang++ texquad-calayer.mm -framework QuartzCore -framework Cocoa -framework OpenGL -o texquad-calayer
+#import  <Cocoa/Cocoa.h>
+#import <QuartzCore/QuartzCore.h>
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/GLU.h>
+#include <OpenGL/GLext.h>
+
+//#include "texquad.cc"
+
+@interface CALayer (Private)
+- (void)setContentsChanged;
+@end
+
+@interface MainView : NSView
+- (void)displayLinkCallback;
+@end
+
+void* io_surface_base_addr = NULL;
+CGLContextObj cgl_context = NULL;
+CGLPixelFormatObj cgl_pixel_format = NULL;
+GLuint gl_texture = 0;
+GLuint gl_fbo = 0;
+CGContextRef cg_context = NULL;
+CGImageRef cg_image = NULL;
+IOSurfaceRef io_surface = NULL;
+
+const float scale_factor = 2;
+const int width = 1000;
+const int height = 640;
+const int px_width = 2000;
+const int px_height = 1280;
+
+MainView* main_view = nil;
+CALayer* main_layer = nil;
+CVDisplayLinkRef g_display_link = NULL;
+
+static CVReturn DisplayLinkCallback(
+    CVDisplayLinkRef displayLink, const CVTimeStamp* now, const CVTimeStamp* outputTime,
+    CVOptionFlags flagsIn, CVOptionFlags* flagsOut, void* displayLinkContext) {
+  [main_view performSelectorOnMainThread:@selector(displayLinkCallback)
+                            withObject:nil
+                         waitUntilDone:NO];
+  return kCVReturnSuccess;
+}
+
+void DisplayLinkInit() {
+  CVDisplayLinkCreateWithActiveCGDisplays(&g_display_link);
+  CVDisplayLinkSetOutputCallback(g_display_link, &DisplayLinkCallback, NULL);
+  CVDisplayLinkStart(g_display_link);
+}
+
+@implementation MainView
+
+- (void)displayLinkCallback {
+  [self performSelector:@selector(updateFrame) withObject:nil afterDelay:0.5/60.];
+}
+
+- (void)updateFrame {
+  CGLSetCurrentContext(cgl_context);
+  //TexQuadInit();
+  //MakeBufferCurrent();
+  //DrawTiles();
+  glBindFramebuffer(GL_FRAMEBUFFER, gl_fbo);
+
+  CFAbsoluteTime timeInSeconds = CFAbsoluteTimeGetCurrent();
+  double green = fmod(timeInSeconds, 25.0) / 25.0;
+  double red = fmod(timeInSeconds, 10.0) / 10.0;
+  glClearColor(red, green, 0.675f, 1.0f);
+  printf("R:%.3f G:%.3f\n", red, green);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  //DrawBuffer();
+  glFlush();
+  glBindFramebuffer(GL_FRAMEBUFFER, 0);
+  CGLSetCurrentContext(NULL);
+
+  [main_layer setContentsChanged];
+}
+
+@end
+
+int main(int argc, char* argv[]) {
+  [NSApplication sharedApplication];
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+  NSMenu* menubar = [NSMenu alloc];
+  [NSApp setMainMenu:menubar];
+
+  NSWindow* window = [[NSWindow alloc]
+      initWithContentRect:NSMakeRect(0, 0, 1000, 640)
+                styleMask:NSWindowStyleMaskTitled
+                  backing:NSBackingStoreBuffered
+                    defer:NO];
+  NSView* view = [window contentView];
+  [window setFrameOrigin:NSMakePoint(20, 20)];
+  [window setTitle:@"Textured Quad with CALayer and CGImage"];
+  [window makeKeyAndOrderFront:nil];
+
+  NSOpenGLPixelFormatAttribute pixelFormatAttributes[] = {
+      NSOpenGLPFAColorSize, 24, NSOpenGLPFAAlphaSize, 8, NSOpenGLPFAAccelerated, 0
+  };
+  NSOpenGLPixelFormat *pixelFormat = [[NSOpenGLPixelFormat alloc]
+      initWithAttributes:pixelFormatAttributes];
+  main_view = [[MainView alloc]
+      initWithFrame:[view bounds]];
+
+  [view setWantsLayer:YES];
+
+  main_layer = [[CALayer alloc] init];
+  [main_layer setBackgroundColor:CGColorGetConstantColor(kCGColorBlack)];
+  [main_layer setFrame:CGRectMake(0, 0, 1000, 640)];
+  [[view layer] addSublayer:main_layer];
+
+  // Create an IOSurface.
+  {
+    unsigned pixelFormat = 'R10k'; //'BGRA'; // 'R10k';
+    unsigned bytesPerElement = 4;
+
+    size_t bytesPerRow = IOSurfaceAlignProperty(kIOSurfaceBytesPerRow, px_width * bytesPerElement);
+    size_t totalBytes = IOSurfaceAlignProperty(kIOSurfaceAllocSize, px_height * bytesPerRow);
+    NSDictionary *options = @{
+        (id)kIOSurfaceWidth: @(px_width),
+        (id)kIOSurfaceHeight: @(px_height),
+        (id)kIOSurfacePixelFormat: @(pixelFormat),
+        (id)kIOSurfaceBytesPerElement: @(bytesPerElement),
+        (id)kIOSurfaceBytesPerRow: @(bytesPerRow),
+        (id)kIOSurfaceAllocSize: @(totalBytes),
+    };
+    io_surface = IOSurfaceCreate((CFDictionaryRef)options);
+    printf("io_surface: %p\n", io_surface);
+  }
+
+  // Create a GL context.
+  {
+    CGLPixelFormatAttribute attribs[] = {static_cast<CGLPixelFormatAttribute>(0)};
+    GLint number_virtual_screens = 0;
+    CGLChoosePixelFormat(attribs, &cgl_pixel_format, &number_virtual_screens);
+
+    CGLCreateContext(cgl_pixel_format, NULL, &cgl_context);
+    printf("cgl_context: %p\n", cgl_context);
+
+    CGLSetCurrentContext(cgl_context);
+    glGenTextures(1, &gl_texture);
+    glBindTexture(GL_TEXTURE_RECTANGLE_ARB, gl_texture);
+    glTexParameterf(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameterf(GL_TEXTURE_RECTANGLE_ARB, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    CGLError cgl_error = CGLTexImageIOSurface2D(
+        CGLGetCurrentContext(),
+        GL_TEXTURE_RECTANGLE_ARB,
+        GL_RGBA,
+        px_width,
+        px_height,
+        GL_BGRA,
+        GL_UNSIGNED_INT_2_10_10_10_REV, //GL_UNSIGNED_INT_8_8_8_8_REV,  //GL_UNSIGNED_INT_2_10_10_10_REV,
+        io_surface,
+        0 /* plane */);
+    printf("CGLTexImageIOSurface2D %d\n", cgl_error);
+
+    glGenFramebuffers(1, &gl_fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, gl_fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_RECTANGLE_ARB, gl_texture, 0);
+    printf("FBO complete: %d\n", glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
+
+    glClearColor(0.5, 0.5, 0.5, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glFlush();
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    CGLSetCurrentContext(NULL);
+    printf("GL error at initial clear: %d\n", glGetError());
+  }
+
+  // Create a CGImage from the IOSurface.
+  {
+    [main_layer setContents:(id)io_surface];
+    [main_layer setBounds:CGRectMake(0, 0, width, height)];
+    [main_layer setContentsScale:scale_factor];
+  }
+
+  [view addSubview:main_view];
+
+  DisplayLinkInit();
+
+  [NSApp activateIgnoringOtherApps:YES];
+  [NSApp run];
+  return 0;
+}
+
