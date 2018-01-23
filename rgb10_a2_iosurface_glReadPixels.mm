af3d4a421ecc131a49e1ab608007a1d9a191b9bd
Miguel Casas-Sanchez <mcasas@chromium.org>
2018-01-23 17:51:04 -0500

    better

--- rgb10_a2_iosurface_glReadPixels.mm
+++ rgb10_a2_iosurface_glReadPixels.mm
@@ -134,182 +134,193 @@ int main(int argc, char* argv[]) {
     printf("CGLTexImageIOSurface2D OK\n");
   }
 
-  // Generate a FrameBuffer, link it to our IOSurface-backed Texture.
-  glGenFramebuffers(1, &gl_fbo);
-  glBindFramebuffer(GL_FRAMEBUFFER, gl_fbo);
-  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-                         GL_TEXTURE_RECTANGLE_ARB, gl_texture, 0);
-  if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-    printf("error creating/binding FBO\n");
-    assert(false);
-  } else {
-    printf("FBO complete OK\n");
-  }
-  printf("FBO: %d\n", glGetError());
-
-  GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
-  glDrawBuffers(1, DrawBuffers);
-  printf("glDrawBuffers: %d\n", glGetError());
-
-  // Read back.
-  glReadPixels(0, 0, px_width, px_height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-  printf("glReadPixels: %d\n", glGetError());
-
-  for (int y = 0; y < px_height; ++y) {
-    for (int x = 0; x < px_width; ++x) {
-      const int pixel_index = y * stride + x * 4;
-      assert(pixels[pixel_index] == color[0]);
-      assert(pixels[pixel_index + 1] == color[1]);
-      assert(pixels[pixel_index + 2] == color[2]);
-      assert(pixels[pixel_index + 3] == color[3]);
-    }
-  }
-  printf("Success: all pixels are correctly read back\n");
-
-/*
-  // Generate and bind a FrameBuffer, render to texture type.
-  {
-    GLuint texture = 0;
-    const GLuint target = GL_TEXTURE_2D;
-    glGenTextures(1, &texture);
-    glBindTexture(target, texture);
-    glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexImage2D(target, 0, GL_RGBA, px_width, px_height, 0, GL_RGBA,
-                 GL_UNSIGNED_BYTE, nullptr);
-
-    glGenFramebuffersEXT(1, &gl_fbo);
-    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, gl_fbo);
-    glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0, target,
-                              texture, 0);
-    if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER) !=
-        GL_FRAMEBUFFER_COMPLETE) {
+  if (0) {
+    // Generate a FrameBuffer, link it to our IOSurface-backed Texture.
+    glGenFramebuffers(1, &gl_fbo);
+    glBindFramebuffer(GL_FRAMEBUFFER, gl_fbo);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+                           GL_TEXTURE_RECTANGLE_ARB, gl_texture, 0);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
       printf("error creating/binding FBO\n");
       assert(false);
     } else {
       printf("FBO complete OK\n");
     }
+    printf("FBO: %d\n", glGetError());
 
-    glBindFramebufferEXT(GL_FRAMEBUFFER, 0);
-    glDeleteTextures(1, &texture);
-    assert(gl_fbo);
+    GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+    glDrawBuffers(1, DrawBuffers);
+    printf("glDrawBuffers: %d\n", glGetError());
+
+    // Read back.
+    glReadPixels(0, 0, px_width, px_height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    printf("glReadPixels: %d\n", glGetError());
+
+    for (int y = 0; y < px_height; ++y) {
+      for (int x = 0; x < px_width; ++x) {
+        const int pixel_index = y * stride + x * 4;
+        assert(pixels[pixel_index] == color[0]);
+        assert(pixels[pixel_index + 1] == color[1]);
+        assert(pixels[pixel_index + 2] == color[2]);
+        assert(pixels[pixel_index + 3] == color[3]);
+      }
+    }
+    printf("Success: all pixels are correctly read back\n");
+
+  } else {
+
+    // Generate and bind a FrameBuffer, render to texture type.
+    {
+      GLuint texture = 0;
+      const GLuint target = GL_TEXTURE_2D;
+      glGenTextures(1, &texture);
+      glBindTexture(target, texture);
+      glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+      glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+      glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+      glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+      glTexImage2D(target, 0, GL_RGBA, px_width, px_height, 0, GL_RGBA,
+                   GL_UNSIGNED_BYTE, nullptr);
+
+      glGenFramebuffersEXT(1, &gl_fbo);
+      glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, gl_fbo);
+      glFramebufferTexture2DEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0,
+                                target, texture, 0);
+      if (glCheckFramebufferStatusEXT(GL_FRAMEBUFFER) !=
+          GL_FRAMEBUFFER_COMPLETE) {
+        printf("error creating/binding FBO\n");
+        assert(false);
+      } else {
+        printf("FBO complete OK\n");
+      }
+
+      glBindFramebufferEXT(GL_FRAMEBUFFER, 0);
+      glDeleteTextures(1, &texture);
+      assert(gl_fbo);
+    }
+
+    glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, gl_fbo);
+    glClearColor(0.0, 0.0, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    printf("GL error at initial clear: %d\n", glGetError());
+
+    glViewport(0, 0, px_width, px_height);
+
+    //////////////////////////////////////////////////////////////////////////////
+    static const char* vertex_shader_code[] = {
+       "#version 150\n\
+        in vec2 a_position;\n\
+        out vec2 v_texCoord;\n\
+        void main() {\n\
+          gl_Position = vec4(a_position.x, a_position.y, 0.0, 1.0);\n\
+          v_texCoord = (a_position + vec2(1.0, 1.0)) * 0.5;\n\
+        }"};
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, vertex_shader_code, nullptr);
+    printf("glShaderSource: %d\n", glGetError());
+    glCompileShader(vertex_shader);
+    printf("glCompileShader: %d\n", glGetError());
+
+    GLint value = 0;
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &value);
+    if (!value) {
+      char buffer[1024] = {0};
+      GLsizei length = 0;
+      glGetShaderInfoLog(vertex_shader, sizeof(buffer), &length, buffer);
+      printf("Error compiling |vertex_shader|: %s\n", buffer);
+      glDeleteShader(vertex_shader);
+      assert(false);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    std::string fragment_shader_code =
+        "#version 150\n"
+        "#define SamplerType sampler2DRect\n"
+        "#define TextureLookup texture\n"
+        "#define TextureScale vec2(256.000000, 256.000000)\n"
+        "uniform SamplerType a_texture;\n"
+        "in vec2 v_texCoord;\n"
+        "out vec4 my_FragData;\n"
+        "void main() {\n"
+        "  my_FragData = TextureLookup(a_texture, v_texCoord * TextureScale);\n"
+        "}";
+    const GLchar *fragment_shader_code_src =
+        reinterpret_cast<const GLchar *>(fragment_shader_code.c_str());
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_code_src, nullptr);
+    glCompileShader(fragment_shader);
+
+    value = 0;
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &value);
+    if (!value) {
+      char buffer[1024] = {0};
+      GLsizei length = 0;
+      glGetShaderInfoLog(fragment_shader, sizeof(buffer), &length, buffer);
+      printf("Error compiling |fragment_shader|: %s\n", buffer);
+      glDeleteShader(fragment_shader);
+      assert(false);
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+
+    GLint linked = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, &linked);
+    if (!linked) {
+      char buffer[1024];
+      GLsizei length = 0;
+      glGetProgramInfoLog(program, sizeof(buffer), &length, buffer);
+      std::string log(buffer, length);
+      printf("Error linking |program|: %s\n", buffer);
+      glDeleteProgram(program);
+      assert(false);
+    }
+
+    glUseProgram(program);
+
+    GLint sampler_location = glGetUniformLocation(program, "a_texture");
+    assert(sampler_location != -1);
+    glUniform1i(sampler_location, 0);
+
+    GLuint vertex_buffer = 0;
+    glGenBuffersARB(1, &vertex_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+    printf("glBindBuffer: %d\n", glGetError());
+
+    GLfloat quad[] = {-1.f, -1.f,
+                       1.f, -1.f,
+                      -1.f,  1.f,
+                       1.f,  1.f};
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quad), quad, GL_STATIC_DRAW);
+    printf("glBufferData: %d\n", glGetError());
+
+    glActiveTexture(gl_texture);
+
+    //glEnableClientState(GL_VERTEX_ARRAY);
+    //printf("glEnableClientState: %d\n", glGetError());
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    printf("glDrawArrays: %d\n", glGetError());
+
+    glReadPixels(0, 0, px_width, px_height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    printf("glReadPixels: %d\n", glGetError());
+
+
+    for (int y = 0; y < px_height; ++y) {
+      for (int x = 0; x < px_width; ++x) {
+        const int pixel_index = y * stride + x * 4;
+        assert(pixels[pixel_index] == color[0]);
+        assert(pixels[pixel_index + 1] == color[1]);
+        assert(pixels[pixel_index + 2] == color[2]);
+        assert(pixels[pixel_index + 3] == color[3]);
+      }
+    }
+    printf("Success: all pixels are correctly read back\n");
   }
-
-  glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, gl_fbo);
-  glClearColor(0.0, 0.0, 1.0f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
-  printf("GL error at initial clear: %d\n", glGetError());
-
-  glViewport(0, 0, px_width, px_height);
-
-  //////////////////////////////////////////////////////////////////////////////
-  static const char* vertex_shader_code[] = {
-     "#version 150\n\
-      in vec2 a_position;\n\
-      out vec2 v_texCoord;\n\
-      void main() {\n\
-        gl_Position = vec4(a_position.x, a_position.y, 0.0, 1.0);\n\
-        v_texCoord = (a_position + vec2(1.0, 1.0)) * 0.5;\n\
-      }"};
-
-  GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-  glShaderSource(vertex_shader, 1, vertex_shader_code, nullptr);
-  printf("glShaderSource: %d\n", glGetError());
-  glCompileShader(vertex_shader);
-  printf("glCompileShader: %d\n", glGetError());
-
-  GLint value = 0;
-  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &value);
-  if (!value) {
-    char buffer[1024] = {0};
-    GLsizei length = 0;
-    glGetShaderInfoLog(vertex_shader, sizeof(buffer), &length, buffer);
-    printf("Error compiling |vertex_shader|: %s\n", buffer);
-    glDeleteShader(vertex_shader);
-    assert(false);
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-  std::string fragment_shader_code =
-      "#version 150\n"
-      "#define SamplerType sampler2DRect\n"
-      "#define TextureLookup texture\n"
-      "#define TextureScale vec2(256.000000, 256.000000)\n"
-      "uniform SamplerType a_texture;\n"
-      "in vec2 v_texCoord;\n"
-      "out vec4 my_FragData;\n"
-      "void main() {\n"
-      "  my_FragData = TextureLookup(a_texture, v_texCoord * TextureScale);\n"
-      "}";
-  const GLchar *fragment_shader_code_src =
-      reinterpret_cast<const GLchar *>(fragment_shader_code.c_str());
-
-  GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment_shader, 1, &fragment_shader_code_src, nullptr);
-  glCompileShader(fragment_shader);
-
-  value = 0;
-  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &value);
-  if (!value) {
-    char buffer[1024] = {0};
-    GLsizei length = 0;
-    glGetShaderInfoLog(fragment_shader, sizeof(buffer), &length, buffer);
-    printf("Error compiling |fragment_shader|: %s\n", buffer);
-    glDeleteShader(fragment_shader);
-    assert(false);
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-  GLuint program = glCreateProgram();
-  glAttachShader(program, vertex_shader);
-  glAttachShader(program, fragment_shader);
-  glLinkProgram(program);
-
-  GLint linked = 0;
-  glGetProgramiv(program, GL_LINK_STATUS, &linked);
-  if (!linked) {
-    char buffer[1024];
-    GLsizei length = 0;
-    glGetProgramInfoLog(program, sizeof(buffer), &length, buffer);
-    std::string log(buffer, length);
-    printf("Error linking |program|: %s\n", buffer);
-    glDeleteProgram(program);
-    assert(false);
-  }
-
-  glUseProgram(program);
-
-  GLint sampler_location = glGetUniformLocation(program, "a_texture");
-  assert(sampler_location != -1);
-  glUniform1i(sampler_location, 0);
-
-  GLuint vertex_buffer = 0;
-  glGenBuffers(1, &vertex_buffer);
-  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-  printf("glBindBuffer: %d\n", glGetError());
-
-  GLfloat vertex_data[] = {-1.f, -1.f,
-                            1.f, -1.f,
-                           -1.f,  1.f,
-                            1.f,  1.f};
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data,
-               GL_STATIC_DRAW);
-  printf("glBufferData: %d\n", glGetError());
-
-  glVertexPointer(4, GL_FLOAT, 0, 0);
-  printf("glVertexPointer: %d\n", glGetError());
-
-
-  glEnableClientState(GL_VERTEX_ARRAY);
-  printf("glEnableClientState: %d\n", glGetError());
-  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-  printf("glDrawArrays: %d\n", glGetError());
-
-  glReadPixels(0, 0, px_width, px_height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
-  printf("glReadPixels: %d\n", glGetError());
-  */
 
   // TODO: cleanup.
 
